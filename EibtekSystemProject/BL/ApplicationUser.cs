e29beaf05e25ca66440cf0c28a39a3b40a55a51d
Ahmed Mostafa <ahmedmostafa706@gmail.com>
2022-12-08 03:22:13 -0800

    First Upload Of The Project

---
+++ EibtekSystemProject/BL/ApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class ApplicationUser:IdentityUser
+    {
+      
+
+
+       
+        public string Name { get; set; }
+        //public DateTime DateCreated { get; set; }
+        //[NotMapped]
+        //public string RoleId { get; set; }
+        //[NotMapped]
+        //public string Role { get; set; }
+        //[NotMapped]
+        //public IEnumerable<SelectListItem> RoleList { get; set; }
+
+
+    }
+}
---
+++ EibtekSystemProject/EibtekSystemProject/Startup.cs
@@ -0,0 +1,107 @@
+using BL;
+using EibtekSystemProject.Models;
+using EmailService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EibtekSystemProject
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            var emailConfig = Configuration
+         .GetSection("EmailConfiguration")
+         .Get<EmailConfiguration>();
+            services.AddSingleton(emailConfig);
+            services.AddScoped<IEmailSender, EmailSender>();
+            services.AddAuthentication()
+             .AddGoogle(options =>
+             {
+                 options.ClientId = "975214719409-pp37jcmifi7bg33254ve18ku83telt9r.apps.googleusercontent.com";
+                 options.ClientSecret = "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";
+
+
+             });
+
+
+
+            services.AddAuthentication().AddFacebook(options =>
+            {
+                options.AppId = "1387677424973135";
+                options.AppSecret = "de6fc7e479121219c97a2e079eee1b3e";
+            });
+            services.AddControllersWithViews();
+            services.AddDbContext<EibtekSystemDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.Password.RequiredLength = 5;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.User.RequireUniqueEmail = true;
+                options.Password.RequireDigit = false;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromSeconds(30);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.SignIn.RequireConfirmedEmail = true;
+
+            }).AddErrorDescriber<CustomIdentityErrorDescriber>().AddEntityFrameworkStores<EibtekSystemDbContext>().AddDefaultTokenProviders();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseCors(MyAllowSpecificOrigins);
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            //app.UseSession();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}");
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
